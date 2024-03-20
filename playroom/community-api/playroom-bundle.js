7c0a4c5995d5a3e8eedb94ddab6fac6b0399016f
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2024-03-20 10:54:30 -0500

    Update playroom-bundle.js

--- playroom/community-api/playroom-bundle.js
+++ playroom/community-api/playroom-bundle.js
@@ -3,6 +3,6 @@
 const lis = ["https://unpkg.com/react@18.2.0/umd/react.development.js","https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js","https://unpkg.com/playroomkit/multiplayer.umd.js"];
 
 for (let s = 0; s < lis.length; s++) {
-  import lis[s];
+  import(lis[s]);
   console.log("Loading PLAYROOM API: "+lis[s]+" Step: "+(s+1)+"/"+lis.length);
 } console.log("Loaded!")
