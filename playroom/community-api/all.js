5119f93af693181632d57cca9a283ff1544ec654
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2024-03-20 08:33:44 -0500

    Create all.js

---
+++ playroom/community-api/all.js
@@ -0,0 +1,10 @@
+/*/
+This is a community created API that works with playroom. 
+Creator: BigS [[hot]]
+Contributors:
+-
+This is intended to work with Godot ONLY. We might eventually port it to other engines, just godot is what I use.
+
+Apache License, Version 2.0 | See https://opensource.org/license/apache-2-0 for useage
+/*/
+import "module/sam.js";
