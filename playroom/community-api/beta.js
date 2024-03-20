2b73a3e0998efdd8c6c9717992c3f9858c34f7a6
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2024-03-20 08:41:22 -0500

    Update beta.js

--- playroom/community-api/beta.js
+++ playroom/community-api/beta.js
@@ -1,13 +1,13 @@
 /*/
-This is the beta version of the Playroom Community API Toolkit
+This is the beta version of the Playroom Community API Toolkit (Has regular and beta features)
 /*/
 /*/
-This is a community created API that works with playroom. 
+This is a community-created API that works with Playroom. 
 Creator: BigS [[hot]]
 Contributors:
 -
-This is intended to work with Godot ONLY. We might eventually port it to other engines, just godot is what I use.
+This is intended to work with Godot ONLY. We might eventually port it to other engines, just Godot is what I use.
 
-Apache License, Version 2.0 | See https://opensource.org/license/apache-2-0 for useage
+Apache License, Version 2.0 | See https://opensource.org/license/apache-2-0 for usage
 /*/
-import "module/sam.js";
+import "./all.js";
