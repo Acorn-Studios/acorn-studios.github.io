7f3ed0eba2d0320bff9bf9e1f4bb07dc4d48fb38
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2024-03-20 09:32:36 -0500

    Create playroom-bundle.js

---
+++ playroom/community-api/playroom-bundle.js
@@ -0,0 +1,4 @@
+/*/ ALL IN ONE PLAYROOM GODOT IMPORT HEADER PACKAGE /*/
+import "https://unpkg.com/react@18.2.0/umd/react.development.js";
+import "https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js";
+import "https://unpkg.com/playroomkit/multiplayer.umd.js";
