95eeca1cba345a479c461ba599306fdeb94d2e1b
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2024-03-20 10:45:02 -0500

    Update playroom-bundle.js

--- playroom/community-api/playroom-bundle.js
+++ playroom/community-api/playroom-bundle.js
@@ -1,4 +1,8 @@
 /*/ ALL IN ONE PLAYROOM GODOT IMPORT HEADER PACKAGE /*/
-import "https://unpkg.com/react@18.2.0/umd/react.development.js";
-import "https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js";
-import "https://unpkg.com/playroomkit/multiplayer.umd.js";
+
+const lis = ["https://unpkg.com/react@18.2.0/umd/react.development.js","https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js","https://unpkg.com/playroomkit/multiplayer.umd.js"];
+
+for (let s = 0; s < lis.length; s++) {
+  import lis[s];
+  console.log("Loading PLAYROOM API: "+lis[s]+" Step: "+(s+1)+"/"+lis.length);
+} console.log("Loaded!")
