b98007cda6d3673b24b3456eb92d455fcd724aba
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2024-04-29 18:08:08 -0500

    Update agora-all.js

--- playroom/agora-utils/agora-all.js
+++ playroom/agora-utils/agora-all.js
@@ -1,1 +1,3 @@
-import("https://acorn-studios.github.io/playroom/agora-utils/RtcTokenBuilder.js")
+import("https://acorn-studios.github.io/playroom/agora-utils/RtcTokenBuilder.js");
+import("https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
+import("https://unpkg.com/agora-rtc-sdk-ng@4.20.2/AgoraRTC_N-production.js");
